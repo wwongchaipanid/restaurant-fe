075d204dcef4d8abca283c4c326a9188cfade3d8
Ositone watcharapon Wongchaipanid <ositone_watcharapon.wongchaipanid@krungthai.com>
2022-12-28 16:30:35 +0700

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
   }
 ];
 @NgModule({
